d42acbebee09ab3dc76aa7229982316484325d57
Prince <kumarprince143@143gmail.com>
2023-06-04 15:15:47 +0530

    making website responsive

--- static/speedx.js
+++ static/speedx.js
@@ -339,4 +339,17 @@ window.onload = function () {
 
 
 // Customer Testimonial End ======================================
+
+// mobile navbar ======================================================
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "0px";
+  }
   
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+// mobile navbar ===================================================
